46cdf29cde75162e934734aae00505e98b01adb5
biplabCyberSwift <jobs.biplab.sharma@gmail.com>
2025-05-25 21:35:26 +0530

    File crud, create read and create folder

--- src/file.helper.js
+++ src/file.helper.js
@@ -1,4 +1,4 @@
-import { writeFile, readFile, unlink, mkdir, appendFile, copyFile } from 'node:fs/promises';
+import { writeFile, readFile, unlink, mkdir, appendFile, copyFile, access, constants } from 'node:fs/promises';
 import logData from './logger.js'
 
 export class FileHelper {
@@ -9,16 +9,61 @@ export class FileHelper {
         data: undefined,
     }
 
-    constructor(fileParam = {
-        basePath: "",
-        folderName: "",
-        fileName: "",
-        data: undefined,
-    }) {
-        this.#fileParams = fileParam;
+    setBasePath = (path = "") => {
+        this.#fileParams.basePath = path;
     }
 
-    readFileData = () => {
-        logData(this.#fileParams.data);
+    setfolderName = (folderName = "") => {
+        this.#fileParams.folderName = folderName;
+    }
+
+    setfileName = (fileName = "") => {
+        this.#fileParams.fileName = fileName;
+    }
+
+    setData = (data = "") => {
+        this.#fileParams.data = data;
+    }
+
+    checkDirectoryStatus = async (folderPath = undefined) => {
+        try {
+            await access(folderPath || this.#fileParams.basePath, constants.F_OK);
+            return true;
+        } catch (error) {
+            logData(error, "error");
+            return false;
+        }
+    }
+
+    createDirectory = async () => {
+        try {
+            const folderPath = `${this.#fileParams.basePath}\\${this.#fileParams.folderName}`;
+            if (!await this.checkDirectoryStatus(folderPath)) {
+                await mkdir(folderPath, { recursive: true });
+            }
+            return true
+        } catch (error) {
+            logData(error, "error");
+            return false;
+        }
+    }
+
+    createTextFile = async () => {
+        try {
+            const fullFilePath = `${this.#fileParams.basePath}\\${this.#fileParams.folderName}\\${this.#fileParams.fileName}.txt`;
+            const data = this.#fileParams.data;
+            await writeFile(fullFilePath, data, "utf-8");
+            return true
+        } catch (error) {
+            logData(error, "error");
+            return false;
+        }
+
+    }
+
+    readFileData = async () => {
+        const fullFilePath = `${this.#fileParams.basePath}\\${this.#fileParams.folderName}\\${this.#fileParams.fileName}.txt`;
+        const data = await readFile(fullFilePath, { encoding: "utf-8" });
+        return data;
     }
 }
--- src/input.output.helper.js
+++ src/input.output.helper.js
@@ -1,23 +1,88 @@
 import { stdin as input, stdout as output } from "node:process";
-import { Interface, createInterface } from "node:readline/promises";
-import { } from "readline/promises";
-import logData from "./logger.js";
+import { createInterface } from "node:readline/promises";
 import { FileHelper } from "./file.helper.js";
+import logData from "./logger.js";
 
 export class IOHelper {
     #io = createInterface({ input, output });
+    #basePath = "";
+    #folderName = "";
+    #fileName = "";
+    #data = "";
 
     startProcess = async () => {
-        const as = await this.#io.question("Hello sourav!\nHow was your day?");
-        logData(as);
+        const welcomeMessage = `Hello, this is a Node Js console application
+        \nWhere we can read, write and update a file
+        \nby just following the options in the screen\n`;
+        logData(`
+            \n${welcomeMessage}
+        `);
 
-        const fH = new FileHelper({
-            data: as
-        });
-        const yesNo = await this.#io.question("Do You want to read the data?\n Yes or No...");
-        if (yesNo === "Yes")
-            fH.readFileData();
+        //Asking Questions
+        const beginJourneyStatus = await this.#io.question("Do you want to continue?\nYes or No...\n");
+        if (beginJourneyStatus !== "Yes")
+            process.exit(0);
+
+        const fileHelper = new FileHelper();
+
+        this.#basePath = await this.#io.question("Enter the base folder path...\n");
+        fileHelper.setBasePath(this.#basePath);
+
+        const basePathStatus = await fileHelper.checkDirectoryStatus();
+        if (!basePathStatus) {
+            logData(`
+            \nUnable to verify folder, stopping the process, retry later\n
+        `);
+            process.exit(0);
+        }
+
+        this.#folderName = await this.#io.question("Please enter the folderName\n");
+        fileHelper.setfolderName(this.#folderName);
+
+        logData("Creating directory...\n");
+
+        const folderStatus = await fileHelper.createDirectory();
+        if (!folderStatus) {
+            logData("Folder creation failed, stopping the process, retry later...\n");
+            process.exit(0);
+        }
+
+        logData(`Directory created ${this.#basePath}\\${this.#folderName}...\n`);
+
+        this.#fileName = await this.#io.question("Enter the fileName only without extension\n");
+        fileHelper.setfileName(this.#fileName);
+
+        this.#data = await this.#io.question("Enter some text that you want write on the file...\n");
+        fileHelper.setData(this.#data);
+
+        const fileCreationStatus = await fileHelper.createTextFile();
+        if (!fileCreationStatus) {
+            logData("File creation failed, stopping the process, retry later...\n");
+            process.exit(0);
+        }
+
+        logData(`File created ${this.#basePath}\\${this.#folderName}\\${this.#fileName}.txt...\n`);
+        const readFileStatus = await this.#io.question("Do you want to read the file? Yes or No: \n");
+        if (readFileStatus !== "Yes") {
+            logData("Stopping the process, retry later...\n");
+            process.exit(0);
+        }
+
+        const fileData = await fileHelper.readFileData();
+        logData(`Here is your data\n
+                ${fileData}`);
+
+
+        const startOverStatus = await this.#io.question("Do you want to start over or exit? Yes or No: \n");
+        if (startOverStatus !== "Yes") {
+            logData("Stopping the process, retry later...\n");
+            process.exit(0);
+        }
+
+        await this.startProcess();
+
+
+
     }
-
 
 }
